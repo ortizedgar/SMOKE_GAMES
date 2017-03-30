2abc4f1ef386194433c132e0d5011e8cdb7c566b
Edgar Ortiz <ortizedgar@live.com.ar>
2017-03-30 17:15:10 -0300

    Renombramiento de carpetas

---
+++ 2017_1C_0_SmokeGames.Test/GameModelTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2017_1C_0_SmokeGames.Test
+{
+    [TestClass]
+    public class GameModelTest
+    {
+        [TestMethod]
+        public void InitTestOk()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}

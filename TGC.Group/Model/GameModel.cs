83ab577f92af7da09c18cb5033661bd4e3dc3a70
Edgar Alejandro Ortiz <ortizedgar@live.com.ar>
2017-04-01 12:33:03 -0300

    #4 Creado un pasillo con techo, paredes y piso. Agreado movimiento hacia adelante con W y atras con S.

--- TGC.Group/Model/GameModel.cs
+++ TGC.Group/Model/GameModel.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using Microsoft.DirectX;
+using Microsoft.DirectX.DirectInput;
 using TGC.Core.Direct3D;
 using TGC.Core.Example;
 using TGC.Core.Geometry;
@@ -22,9 +25,13 @@ namespace TGC.Group.Model
         private readonly IVector3Factory vector3Factory;
 
         /// <summary>
-        /// Plano que contiene el piso del escenario
+        /// Lista de <see cref="TgcPlane"/> que contiene el piso del escenario
         /// </summary>
-        private TgcPlane floor;
+        private List<TgcPlane> floor;
+
+        private List<TgcPlane> walls;
+
+        private List<TgcPlane> roof;
 
         /// <summary>
         ///     Constructor del juego.
@@ -42,20 +49,98 @@ namespace TGC.Group.Model
 
         /// <summary>
         ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo.
-        ///     Escribir aquí todo el código de inicialización: cargar modelos, texturas, estructuras de optimización, todo procesamiento que podemos pre calcular para nuestro juego.
         /// </summary>
         public override void Init()
         {
-            floor = tgcPlaneFactory.CreateTgcPlane();
-            floor.setTexture(TgcTexture.createTexture(D3DDevice.Instance.Device, MediaDir + @"\stones.bmp"));
-            floor.Origin = vector3Factory.CreateVector3(0, 0, 0);
-            floor.Size = vector3Factory.CreateVector3(1000, 1000, 1000);
-            floor.Orientation = TgcPlane.Orientations.XZplane;
-            floor.AutoAdjustUv = false;
-            floor.UTile = 1;
-            floor.VTile = 1;
+            var camaraLocation = vector3Factory.CreateVector3(5, 2, -1);
+            Camara.SetCamera(camaraLocation, vector3Factory.CreateVector3(camaraLocation.X, camaraLocation.Y, camaraLocation.Z + 1));
 
-            Camara.SetCamera(vector3Factory.CreateVector3(500, 200, -100), vector3Factory.CreateVector3(500, 150, 0));
+            CreateScenario();
+        }
+
+        private void CreateScenario()
+        {
+            CreateFloor();
+
+            CreateWalls();
+
+            CreateRoof();
+        }
+
+        private void CreateRoof()
+        {
+            roof = new List<TgcPlane>();
+            var texture = TgcTexture.createTexture(D3DDevice.Instance.Device, MediaDir + @"\roof.jpg");
+
+            for (var i = 0; i < 10; i++)
+            {
+                var roofElement = tgcPlaneFactory.CreateTgcPlane();
+                roofElement.setTexture(texture);
+                roofElement.Origin = vector3Factory.CreateVector3(0, 10, 10 * i);
+                roofElement.Size = vector3Factory.CreateVector3(10, 10, 10);
+                roofElement.Orientation = TgcPlane.Orientations.XZplane;
+                roofElement.AutoAdjustUv = false;
+                roofElement.UTile = 1;
+                roofElement.VTile = 1;
+                roof.Add(roofElement);
+            }
+        }
+
+        private void CreateWalls()
+        {
+            walls = new List<TgcPlane>();
+            var texture = TgcTexture.createTexture(D3DDevice.Instance.Device, MediaDir + @"\wall.jpg");
+
+            for (var i = 0; i < 10; i++)
+            {
+                if (i % 3 != 0)
+                {
+                    var wallElement = tgcPlaneFactory.CreateTgcPlane();
+                    wallElement.setTexture(texture);
+                    wallElement.Origin = vector3Factory.CreateVector3(0, 0, 10 * i);
+                    wallElement.Size = vector3Factory.CreateVector3(10, 10, 10);
+                    wallElement.Orientation = TgcPlane.Orientations.YZplane;
+                    wallElement.AutoAdjustUv = false;
+                    wallElement.UTile = 1;
+                    wallElement.VTile = 1;
+                    walls.Add(wallElement);
+                }
+            }
+
+            for (var i = 0; i < 10; i++)
+            {
+                if (i != 5)
+                {
+                    var wallElement = tgcPlaneFactory.CreateTgcPlane();
+                    wallElement.setTexture(texture);
+                    wallElement.Origin = vector3Factory.CreateVector3(10, 0, 10 * i);
+                    wallElement.Size = vector3Factory.CreateVector3(10, 10, 10);
+                    wallElement.Orientation = TgcPlane.Orientations.YZplane;
+                    wallElement.AutoAdjustUv = false;
+                    wallElement.UTile = 1;
+                    wallElement.VTile = 1;
+                    walls.Add(wallElement);
+                }
+            }
+        }
+
+        private void CreateFloor()
+        {
+            floor = new List<TgcPlane>();
+            var texture = TgcTexture.createTexture(D3DDevice.Instance.Device, MediaDir + @"\floor.bmp");
+
+            for (var i = 0; i < 10; i++)
+            {
+                var floorElement = tgcPlaneFactory.CreateTgcPlane();
+                floorElement.setTexture(texture);
+                floorElement.Origin = vector3Factory.CreateVector3(0, 0, 10 * i);
+                floorElement.Size = vector3Factory.CreateVector3(10, 10, 10);
+                floorElement.Orientation = TgcPlane.Orientations.XZplane;
+                floorElement.AutoAdjustUv = false;
+                floorElement.UTile = 1;
+                floorElement.VTile = 1;
+                floor.Add(floorElement);
+            }
         }
 
         /// <summary>
@@ -64,6 +149,23 @@ namespace TGC.Group.Model
         public override void Update()
         {
             PreUpdate();
+
+            MoveCamara();
+        }
+
+        private void MoveCamara()
+        {
+            Vector3 cameraPosition;
+            if (Input.keyDown(Key.W))
+            {
+                cameraPosition = vector3Factory.CreateVector3(Camara.Position.X, Camara.Position.Y, Camara.Position.Z + 10 * ElapsedTime);
+                Camara.SetCamera(cameraPosition, vector3Factory.CreateVector3(Camara.Position.X, Camara.Position.Y, Camara.Position.Z + 1));
+            }
+            if (Input.keyDown(Key.S))
+            {
+                cameraPosition = vector3Factory.CreateVector3(Camara.Position.X, Camara.Position.Y, Camara.Position.Z - 10 * ElapsedTime);
+                Camara.SetCamera(cameraPosition, vector3Factory.CreateVector3(Camara.Position.X, Camara.Position.Y, Camara.Position.Z + 1));
+            }
         }
 
         /// <summary>
@@ -74,11 +176,26 @@ namespace TGC.Group.Model
             // Inicio el render de la escena, para ejemplos simples. Cuando tenemos postprocesado o shaders es mejor realizar las operaciones según nuestra conveniencia.
             PreRender();
 
-            // Actualizar valores de pared
-            floor.updateValues();
+            // Renderizar el piso
+            foreach (var floorElement in floor)
+            {
+                floorElement.updateValues();
+                floorElement.render();
+            }
 
-            // Renderizar pared
-            floor.render();
+            // Renderizar las paredes
+            foreach (var wallElement in walls)
+            {
+                wallElement.updateValues();
+                wallElement.render();
+            }
+
+            // Renderizar el techo
+            foreach (var roofElement in roof)
+            {
+                roofElement.updateValues();
+                roofElement.render();
+            }
 
             // Finaliza el render y presenta en pantalla, al igual que el preRender se debe para casos puntuales es mejor utilizar a mano las operaciones de EndScene y PresentScene
             PostRender();
@@ -91,7 +208,11 @@ namespace TGC.Group.Model
         /// </summary>
         public override void Dispose()
         {
-            floor.dispose();
+            // Renderizar el piso
+            foreach (var floorElement in floor)
+            {
+                floorElement.dispose();
+            }
         }
     }
 }
